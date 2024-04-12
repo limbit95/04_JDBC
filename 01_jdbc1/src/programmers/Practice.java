d9525ea2526411c4442b9b83dcdfe7bb3487d9bb
limbit95 <limbit95@gmail.com>
2024-04-12 17:39:33 +0900

    학원

--- 01_jdbc1/src/programmers/Practice.java
+++ 01_jdbc1/src/programmers/Practice.java
@@ -1,7 +1,5 @@
 package programmers;
 
-import java.math.BigInteger;
-import java.text.NumberFormat;
 import java.util.Arrays;
 
 public class Practice {
@@ -9,22 +7,53 @@ public class Practice {
 	public static void main(String[] args) {
 		
 		
-		System.out.println(solution("abstract algebra"));
+		System.out.println(solution("baxbc"));
 		
 	}
 	
 	
-	public static String solution(String myString) {
-        String answer = "";
+	public static String[] solution(String myString) {
+        String[] answer = {};
         
-        for(int i = 0; i < myString.length(); i++) {
-        	if(myString.charAt(i) == 'a') {
-        		answer += myString.toUpperCase().charAt(i);
-        	} else {
-        		answer += myString.toLowerCase().charAt(i);
+        answer = myString.split("x");
+        
+        int[] intArr = new int[answer.length];
+
+        for(int i = 0; i < answer.length; i++) {
+        	int sum = 0;
+        	
+        	for(int x = 0; x < answer[i].length(); x++) {
+        		if(x == 0) {
+        			sum = answer[i].charAt(x);
+        		} else if(answer[i].charAt(x-1) < answer[i].charAt(x)){
+        			continue;
+        		} else if(answer[i].charAt(x-1) >= answer[i].charAt(x)) {
+        			sum += answer[i].charAt(x);
+        		} else {
+        			sum += answer[i].charAt(x);
+        		}
+        	}
+        	intArr[i] = sum;
+        }
+        
+        for(int i = 0; i < intArr.length; i++) {
+        	for(int x = i + 1; x < intArr.length; x++) {
+        		if(intArr[x] < intArr[i]) {
+        			int temp = intArr[i];
+        			intArr[i] = intArr[x];
+        			intArr[x] = temp;
+        			
+        			String tempStr = answer[i];
+        			answer[i] = answer[x];
+        			answer[x] = tempStr;
+        		}
         	}
         }
         
+        System.out.println(Arrays.toString(intArr));
+        System.out.println(Arrays.toString(answer));
+        
         return answer;
     }
+	
 }
